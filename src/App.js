fb63aad4d3f71788fd0626c773e07b662cf854c9
scottA <scot.atkinson@live.co.uk>
2024-02-28 12:27:10 +0000

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [audioSource, setAudioSource] = useState("https://radio.canstream.co.uk:9037/live.mp3");
+  const [playerKey, setPlayerKey] = useState(1);
+
+  const handleRadio1Click = () => {
+    setAudioSource("https://radio.canstream.co.uk:9037/live.mp3");
+    setPlayerKey((prevKey) => prevKey + 1);
+  };
+
+  const handleRadio2Click = () => {
+    setAudioSource("https://s42.myradiostream.com/:29400/listen.mp3?_=372338?nocache=1709122376");
+    setPlayerKey((prevKey) => prevKey + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Radio Player</h1>
+
+        <audio controls key={playerKey}>
+          <source src={audioSource} type="audio/mpeg"></source>
+          Your browser does not support the audio element.
+        </audio>
+
+        <div>
+          <button onClick={handleRadio1Click}>Radio 1</button>
+          <button onClick={handleRadio2Click}>Radio 2</button>
+        </div>
       </header>
     </div>
   );
